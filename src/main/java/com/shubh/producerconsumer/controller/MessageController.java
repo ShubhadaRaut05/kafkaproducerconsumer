369455860ba99fd5e3d2d6646396296523a697ca
shubhadaraut <shubhadamali05@gmail.com>
2024-10-16 23:38:34 +0530

    changes

---
+++ src/main/java/com/shubh/producerconsumer/controller/MessageController.java
@@ -0,0 +1,24 @@
+package com.shubh.producerconsumer.controller;
+
+import com.shubh.producerconsumer.service.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    private final KafkaProducer kafkaProducer;
+
+    @Autowired
+    public MessageController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @PostMapping("/send")
+    public void sendMessage( @RequestBody String message) {
+        kafkaProducer.sendMessage("my-topic", message);
+    }
+}
---
+++ src/main/java/com/shubh/producerconsumer/service/KafkaConsumer.java
@@ -0,0 +1,15 @@
+package com.shubh.producerconsumer.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "my-topic", groupId = "my-group")
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+    }
+}
